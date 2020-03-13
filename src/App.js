3087e6781d12236491af8ba255cae3dd759f107e
unknown <arthurbosah@yahoo.com>
2020-03-13 09:33:00 +0100

    configuring one signal

--- src/App.js
+++ src/App.js
@@ -81,7 +81,11 @@ function App() {
           OneSignal.registerForPushNotifications();
         });
       } else if (permission == "granted") {
-        OneSignal.showSlidedownPrompt();
+        // OneSignal.showSlidedownPrompt();
+        OneSignal.getUserId(function (userId) {
+          console.log("OneSignal User ID:", userId);
+          // (Output) OneSignal User ID: 270a35cd-4dda-4b3f-b04e-41d7463a2316    
+        });
         console.log("this guy don accepted")
       } else if (permission == "denied") {
         OneSignal.showNativePrompt();
